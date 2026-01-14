0ca811177aa11ba3a9f525a9c018b05c76d45d08
Sergey Matylionak <79189126+matowdev@users.noreply.github.com>
2026-01-14 13:50:40 +0300

    [PW-10.8/cont] gallery-list-sort
    Solve 1st task. Work with "DOM, listener, event.target".
    
    QA-SB: B-5 / JS

--- 5-javascript/10-extended-dom/10-8-pw-4-gallery-list-sort/task-1/index.js
+++ 5-javascript/10-extended-dom/10-8-pw-4-gallery-list-sort/task-1/index.js
@@ -10,3 +10,17 @@
 - Если у вас нет отдельных изображений большого размера, то просто используйте URL миниатюры.
 - Выполните минимальную CSS-стилизацию DOM-элементов.
 */
+
+const galleryList = document.querySelector('.gallery-list');
+const galleryPreview = document.querySelector('.gallery-preview');
+
+function addImgToPreview(event) {
+  const target = event.target;
+
+  if (target.tagName === 'IMG') {
+    const targetSrc = target.getAttribute('src');
+    galleryPreview.style.background = `url('${targetSrc}') no-repeat center / cover`;
+  }
+}
+
+galleryList.addEventListener('click', addImgToPreview);
