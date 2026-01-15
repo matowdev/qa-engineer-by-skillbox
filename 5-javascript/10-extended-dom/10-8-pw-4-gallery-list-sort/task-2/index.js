ae8ae9507ded60353c85b2af798d81e14a36e106
Sergey Matylionak <79189126+matowdev@users.noreply.github.com>
2026-01-15 10:32:47 +0300

    [PW-10.8/cont] gallery-list-sort
    Solve 2nd task. Work with "DOM, listeners, append/remove(), ..Child".
    
    QA-SB: B-5 / JS

--- 5-javascript/10-extended-dom/10-8-pw-4-gallery-list-sort/task-2/index.js
+++ 5-javascript/10-extended-dom/10-8-pw-4-gallery-list-sort/task-2/index.js
@@ -6,3 +6,41 @@
 - Под списком добавьте кнопку «Удалить», которая удаляет последний элемент списка.
 - Выполните минимальную CSS-стилизацию DOM-элементов.
 */
+
+const page = document.querySelector('.page');
+
+const list = document.createElement('ul');
+list.classList.add('list');
+
+const btnsWrap = document.createElement('div');
+btnsWrap.classList.add('btns-wrap');
+
+const addBtn = document.createElement('button');
+addBtn.classList.add('btn', 'add-btn');
+addBtn.textContent = 'Добавить элемент';
+
+const deleteBtn = document.createElement('button');
+deleteBtn.classList.add('btn', 'delete-btn');
+deleteBtn.textContent = 'Удалить элемент';
+
+btnsWrap.append(addBtn, deleteBtn);
+page.append(list, btnsWrap);
+
+function addItem() {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = 'Новый элемент списка';
+
+  list.append(itemEl);
+}
+
+function deleteItem() {
+  const lastItemEl = list.lastElementChild;
+
+  if (lastItemEl) {
+    lastItemEl.remove();
+  }
+}
+
+addBtn.addEventListener('click', addItem);
+deleteBtn.addEventListener('click', deleteItem);
