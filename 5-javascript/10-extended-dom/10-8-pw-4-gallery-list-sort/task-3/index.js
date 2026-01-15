d1eec359e86000b545246461b52ddb2f29ba7d13
Sergey Matylionak <79189126+matowdev@users.noreply.github.com>
2026-01-15 11:44:51 +0300

    [PW-10.8/compl] gallery-list-sort
    Solve 3rd task. Work with "DOM, listeners, ..Fragment, sort()".
    
    Worth noting:
      - all this work (3 tasks).
    
    QA-SB: B-5 / JS

--- 5-javascript/10-extended-dom/10-8-pw-4-gallery-list-sort/task-3/index.js
+++ 5-javascript/10-extended-dom/10-8-pw-4-gallery-list-sort/task-3/index.js
@@ -12,3 +12,56 @@
   - обработчики событий для кнопок сортировки, которые будут вызывать соответствующие функции сортировки и обновлять отображаемый список.
 3. Выполните минимальную CSS-стилизацию DOM-элементов.
 */
+
+const priceList = document.querySelector('.price-list');
+const btnSortUp = document.querySelector('.btn-sort-up');
+const btnSortDown = document.querySelector('.btn-sort-down');
+
+const prices = [100, 500, 250, 750, 300];
+
+function renderPriceList(arr) {
+  if (!arr || !Array.isArray(arr)) {
+    return;
+  }
+
+  priceList.innerHTML = '';
+  const documentFragment = document.createDocumentFragment();
+
+  arr.forEach((price) => {
+    const priceItem = document.createElement('li');
+    priceItem.classList.add('price-item');
+    priceItem.textContent = price;
+
+    documentFragment.append(priceItem);
+  });
+
+  priceList.append(documentFragment);
+}
+
+renderPriceList(prices); // сразу отрисовка default массива/списка
+
+function sortUp(arr) {
+  if (!arr || !Array.isArray(arr)) {
+    return;
+  }
+
+  return [...arr].sort((a, b) => a - b);
+}
+
+function sortDown(arr) {
+  if (!arr || !Array.isArray(arr)) {
+    return;
+  }
+
+  return [...arr].sort((a, b) => b - a);
+}
+
+btnSortUp.addEventListener('click', () => {
+  const sortedUpArr = sortUp(prices);
+  renderPriceList(sortedUpArr);
+});
+
+btnSortDown.addEventListener('click', () => {
+  const sortedDownArr = sortDown(prices);
+  renderPriceList(sortedDownArr);
+});
