e18f69dd9884811ed0da4caad81689b0249821cf
Sergey Matylionak <79189126+matowdev@users.noreply.github.com>
2026-01-22 14:04:21 +0300

    [PW-11.9/cont] survey-shipping-card
    Solve 1st task. Work with "DOM/form, inputs/value, innerHTML".
    
    QA-SB: B-5 / JS

--- 5-javascript/11-working-with-forms/11-9-pw-5-survey-shipping-card/task-1/index.js
+++ 5-javascript/11-working-with-forms/11-9-pw-5-survey-shipping-card/task-1/index.js
@@ -1,3 +1,67 @@
 'use strict';
 
 // Создайте веб-страницу с формой опроса для сбора данных о пользователе. В момент ввода часть полей проверяется на правильность заполнения пользователем. После заполнения формы информация о пользователе выводится на экран (подробнее, см. страницу практической работы).
+
+const form = document.getElementById('form');
+const rangeInput = document.getElementById('range');
+const rangeValue = document.getElementById('range-value');
+
+rangeInput.addEventListener('input', () => {
+  rangeValue.textContent = rangeInput.value;
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const name = document.getElementById('text').value;
+  const email = document.getElementById('email').value;
+
+  const genderInput = form.querySelector('input[name="gender"]:checked');
+  let genderText = 'Не указан';
+
+  if (genderInput) {
+    const label = form.querySelector(`label[for="${genderInput.id}"]`);
+    genderText = label.textContent;
+  }
+
+  const rating = rangeInput.value;
+
+  const checkedInterests = form.querySelectorAll(
+    'input[name="sport"]:checked, input[name="music"]:checked, input[name="travel"]:checked, input[name="movie"]:checked',
+  );
+
+  const interestsList = [];
+
+  checkedInterests.forEach((interest) => {
+    const label = form.querySelector(`label[for="${interest.id}"]`);
+    interestsList.push(label.textContent);
+  });
+
+  const interestsResult =
+    interestsList.length > 0 ? interestsList.join(', ') : 'Нет интересов';
+
+  const comment = document.getElementById('textarea').value;
+
+  const existingResult = document.getElementById('result-container');
+
+  if (existingResult) {
+    existingResult.remove();
+  }
+
+  const resultContainer = document.createElement('div');
+  resultContainer.id = 'result-container';
+  resultContainer.style.marginTop = '30px';
+  resultContainer.style.maxWidth = '350px';
+
+  resultContainer.innerHTML = `
+    <h2 style="margin-bottom: 20px; font-weight: 600; font-size: 24px; color: #459b52">Результаты опроса:</h2>
+    <p style="margin-bottom: 10px;"><b>Имя пользователя:</b> ${name}</p>
+    <p style="margin-bottom: 10px;"><b>Email:</b> ${email}</p>
+    <p style="margin-bottom: 10px;"><b>Пол:</b> ${genderText}</p>
+    <p style="margin-bottom: 10px;"><b>Оценка сервиса:</b> ${rating}</p>
+    <p style="margin-bottom: 10px;"><b>Интересы пользователя:</b> ${interestsResult}</p>
+    <p style="margin-bottom: 10px;"><b>Дополнительные комментарии:</b> ${comment ? comment : 'Нет комментариев'}</p>
+  `;
+
+  form.after(resultContainer);
+});
