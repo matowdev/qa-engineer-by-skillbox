0d83084a79fb23615eaa2ff1e948e954e6141fc9
Sergey Matylionak <79189126+matowdev@users.noreply.github.com>
2026-01-27 15:11:34 +0300

    [PW-12.8/compl] popup-promo
    Solve 2nd task. Work with "DOM, Cookie, reduce()".
    
    Worth noting:
      - all this work (2 tasks).
    
    QA-SB: B-5 / JS

--- 5-javascript/12-built-in-objects-and-functions/12-8-pw-6-popup-promo/task-2/index.js
+++ 5-javascript/12-built-in-objects-and-functions/12-8-pw-6-popup-promo/task-2/index.js
@@ -1,3 +1,65 @@
 'use strict';
 
 // Создайте для потенциального интернет-магазина форму для ввода подарочного промокода с возможностью проверки введённого значения.
+// !! Уточнение: чтобы корректно взаимодействовать с cookie браузера, нужно работать через "Live Server"
+
+const promocodeObj = {
+  promocode: 'PROM50',
+  gift: 'Скидка 50%',
+};
+
+const promoForm = document.querySelector('.promo__form');
+const promoInput = promoForm.querySelector('.promo__input');
+const promoStatus = promoForm.querySelector('.promo__status');
+
+function getCookie() {
+  return document.cookie.split('; ').reduce((acc, item) => {
+    const [name, value] = item.split('=');
+    acc[name] = value;
+
+    return acc;
+  }, {});
+}
+
+function activatePromo(giftText) {
+  if (!giftText) {
+    return;
+  }
+
+  promoStatus.textContent = `Промокод применён. ${giftText}`;
+  promoStatus.classList.add('promo__status_active', 'promo__status_success');
+  promoInput.classList.add('promo__input_success');
+  promoInput.value = promocodeObj.promocode;
+}
+
+function resetPromo() {
+  promoStatus.textContent = '';
+  promoStatus.classList.remove('promo__status_active', 'promo__status_success');
+  promoInput.classList.remove('promo__input_success');
+}
+
+function submitPromo(event) {
+  event.preventDefault();
+
+  const userValue = promoInput.value.trim().toUpperCase();
+
+  if (userValue === promocodeObj.promocode) {
+    activatePromo(promocodeObj.gift);
+    document.cookie = `promocode=${promocodeObj.promocode}; max-age=3600; path=/`; // указываем время хранение cookie.. (max-age=3600, это 1 час)
+  } else {
+    resetPromo();
+    promoInput.value = '';
+    document.cookie = `promocode=; max-age=0; path=/`; // очищаем cookie после некорректного ввода (что бы после reload не вывести что было успешным ранее)
+  }
+}
+
+function checkCookie() {
+  const cookies = getCookie();
+
+  if (cookies.promocode === promocodeObj.promocode) {
+    activatePromo(promocodeObj.gift);
+  }
+}
+
+checkCookie(); // сразу проверка cookie
+promoForm.addEventListener('submit', submitPromo);
